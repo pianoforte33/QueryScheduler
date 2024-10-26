7b68072192a8609e0ca8e6cd351abd236cfada00
Simone <floriosimone6@protonmail.com>
2024-10-26 23:50:34 +0200

    Delete QueryScheduler/build directory
    deleted realease directory

--- QueryScheduler/build/Desktop-Release/CMakeFiles/QueryScheduler.dir/QueryScheduler_autogen/EWIEGA46WW/qrc_queryScheduler.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/QueryScheduler.dir/QueryScheduler_autogen/EWIEGA46WW/qrc_queryScheduler.cpp.o: \
- /run/media/simone/scheda\ SD/qt/QueryScheduler/QueryScheduler/build/Desktop-Release/QueryScheduler_autogen/EWIEGA46WW/qrc_queryScheduler.cpp \
- /usr/include/stdc-predef.h
